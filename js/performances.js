3f018a8c2e0e0257b9d2b434519268e4fb8900cf
Whireless <83311177+Whireless@users.noreply.github.com>
2023-08-26 17:46:40 +0000

    Deploying to gh-pages from @ Whireless/Grand-Theatre@af8bbd6f7669add02346356c83bdfd47fa8f04c0 🚀

---
+++ js/performances.js
@@ -0,0 +1,1 @@
+import{Swiper}from"./swiper.js";const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!1,effect:"slide",breakpoints:{768:{slidesPerView:2}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",bulletElement:"span",type:"bullets"}});
